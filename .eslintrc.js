7ae6b9d1e1fed9a24933aaef8ed7917ec2e88fa0
Johan Svensson <fredrik.svensson@chghealthcare.com>
2020-03-19 10:13:11 -0600

    delint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
-module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  globals: {
-    __resources: true
-  },
-  parserOptions: {
-    sourceType: 'module',
-    parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs'
+// module.exports = {
+//   root: true,
+//   env: {
+//     browser: true,
+//     node: true
+//   },
+//   globals: {
+//     __resources: true
+//   },
+//   parserOptions: {
+//     sourceType: 'module',
+//     parser: 'babel-eslint'
+//   },
+  // extends: [
+  //   '@nuxtjs'
     
-  ],
+  // ],
   // add your custom rules here
-  rules: {
+  //rules: {
     // StandardJS — The Rules
         // "indent": ["error", 2], // 2 spaces – for indentation
         // // "max-len": ["error", { "code": 120}],
@@ -35,5 +35,5 @@ module.exports = {
 
         // "vue/max-attributes-per-line": "off",
         // "vue/singleline-html-element-content-newline" : 0
-  }
-}
+//   }
+// }
