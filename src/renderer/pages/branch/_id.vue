4bc4a88f4a47881b27a265fc07d897eaa24f65d2
Johan Svensson <fredrik.svensson@chghealthcare.com>
2020-03-20 11:05:40 -0600

    added commit message and mock checkbox

--- src/renderer/pages/branch/_id.vue
+++ src/renderer/pages/branch/_id.vue
@@ -8,10 +8,31 @@
     <div class="row">
         <div class="col">
             <b-table head-variant="light" striped hover :items="diff" :fields="fields">
+            <template v-slot:cell(checkbox)="row">
+                <b-form-checkbox name="checkbox" value="true">
+    </b-form-checkbox>
+            </template>
+
             <template  v-slot:cell(fileDiff)="row">
-                <!-- <prism class="diff-highlight" language="diff">{{row.item.fileDiff}}</prism> -->
+                <prism class="diff-highlight" language="diff">{{row.item.fileDiff}}</prism>
             </template>
             </b-table>
+
+            <h5>Commit message (required):</h5>
+
+            <b-form-textarea
+                id="textarea"
+                v-model="text"
+                placeholder="Enter something..."
+                rows="3"
+                max-rows="6"
+                ></b-form-textarea>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col my-2">
+            <b-button>Commit</b-button>
+            <b-button>Cancel</b-button>
         </div>
     </div>
 </div>
@@ -19,17 +40,17 @@
 
 <script>
     const { ipcRenderer } = require('electron')
-    // import 'prismjs'
-    // import 'prismjs/themes/prism-coy.css'
-    // import 'prismjs/components/prism-diff'
-    // import 'prismjs/plugins/diff-highlight/prism-diff-highlight.css'
-    // import Prism from 'vue-prism-component'
+    import 'prismjs'
+    import 'prismjs/themes/prism-coy.css'
+    import 'prismjs/components/prism-diff'
+    import 'prismjs/plugins/diff-highlight/prism-diff-highlight.css'
+    import Prism from 'vue-prism-component'
     export default {
         name: "BranchDetail",
-        // components: {Prism},
+        components: {Prism},
         data() {
             return {
-                fields: [{key:'changes', sortable:true}, {key:'fileDiff'}],
+                fields: ['checkbox',{key:'changes', sortable:false}, {key:'fileDiff'}],
                 diff: []
             };
         },
